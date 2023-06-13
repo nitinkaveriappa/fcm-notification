3bffe4cc6e0a12367e417051e88b2e43482b866c
Nitin Kaveriappa <nitinkaveriappa@gmail.com>
2023-06-13 12:23:17 +0530

    Node JS Script

---
+++ node-js-script/app.js
@@ -0,0 +1,31 @@
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getMessaging } = require("firebase-admin/messaging");
+const serviceAccount = require("./service_account_private_key_file.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const registrationToken =
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+const message = {
+  data: {
+    title: "TESTTITLE",
+    body: "TESTBODY",
+    url: "TESTURL",
+  },
+  token: registrationToken,
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+getMessaging()
+  .send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log("Successfully sent message:", response);
+  })
+  .catch((error) => {
+    console.log("Error sending message:", error);
+  });
